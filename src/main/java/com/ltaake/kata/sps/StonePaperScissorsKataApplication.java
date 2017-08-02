07b3a85bb19626e799cbe623b94a4463a40478df
Lukas Taake <lukas.taake@gmail.com>
2017-08-02 20:57:45 +0200

    Initial import

---
+++ src/main/java/com/ltaake/kata/sps/StonePaperScissorsKataApplication.java
@@ -0,0 +1,12 @@
+package com.ltaake.kata.sps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonePaperScissorsKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonePaperScissorsKataApplication.class, args);
+	}
+}
