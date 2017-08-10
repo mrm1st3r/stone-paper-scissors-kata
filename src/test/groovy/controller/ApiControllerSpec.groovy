27f04c23246a0d5d9e9ae6c2b378754b74347b84
Lukas Taake <lukas.taake@gmail.com>
2017-08-10 11:50:12 +0200

    Fixed executing integrations tests with maven

--- src/test/groovy/controller/ApiControllerSpec.groovy
+++ src/test/groovy/controller/ApiControllerSpec.groovy
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -18,12 +19,16 @@ class ApiControllerSpec extends Specification {
     @Autowired
     MockMvc mvc
 
-    def "should reach REST endpoint"() {
+    @Unroll
+    def "should reach #name REST endpoint"() {
         expect:
         mvc.perform(
-                post("/play").content("\"STONE\"").contentType(MediaType.APPLICATION_JSON)
+                post("/play/" + name).content("\"STONE\"").contentType(MediaType.APPLICATION_JSON)
         ).andExpect(
                 status().isOk()
         )
+
+        where:
+        name << ["original", "wellAdded"]
     }
 }
